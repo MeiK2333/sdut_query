52d06040c4d72ce1987137e23c58cf3defe77738
MeiK <meik2333@gmail.com>
2018-01-20 15:47:13 +0800

    添加教学综合信息服务平台的爬虫，可以获取指定学期的课程表

--- sdut_query/__init__.py
+++ sdut_query/__init__.py
@@ -3,3 +3,4 @@ from .auth_server import AuthServer
 from .ehall import Ehall
 from .ecard import Ecard
 from .lib import Lib
+from .jwglxt import Jwglxt
--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 # coding=utf-8
-from sdut_query import AuthServer, Ehall, Ecard, Lib
+from sdut_query import AuthServer, Ehall, Ecard, Lib, Jwglxt
 
 if __name__ == '__main__':
-    a = AuthServer()
-    username = input('username: ')
-    password = input('password: ')
-    print('AuthServer login:', a.login(username, password))
+    # a = AuthServer()
+    # username = input('username: ')  # 山东理工大学统一登录中心账号密码
+    # password = input('password: ')  # http://ehall.sdut.edu.cn/new/ehall.html
+    # print('AuthServer login:', a.login(username, password))
+    #
+    # e = Ehall(a)
+    # print('Ehall login:', e.login())
+    # print('name:', e.get_name())
+    # print('dorm health', e.get_dorm_health())
+    #
+    # c = Ecard(e)
+    # print('Ecard login:', c.login())
+    # print('balance:', c.balance())
+    # # print('consume:', c.consume_info())
+    #
+    # l = Lib(a)
+    # print('Lib login:', l.login())
+    # print('borrow:', l.get_borrow())
+    # print('borrow info:', l.get_borrow_info())
 
-    e = Ehall(a)
-    print('Ehall login:', e.login())
-    print('name:', e.get_name())
-    print('dorm health', e.get_dorm_health())
-
-    c = Ecard(e)
-    print('Ecard login:', c.login())
-    print('balance:', c.balance())
-    # print('consume:', c.consume_info())
-
-    l = Lib(a)
-    print('Lib login:', l.login())
-    print('borrow:', l.get_borrow())
-    print('borrow info:', l.get_borrow_info())
-
-    a.logout()
+    # a.logout()
 
     # a = AuthServer()
     # a.login(username, password)
@@ -33,3 +33,9 @@ if __name__ == '__main__':
     #     print('login:', a.login(username, password))
     #
     # a.logout()
+
+    username = input('username: ')  # 山东理工大学教学综合信息服务平台
+    password = input('password: ')  # http://211.64.28.123
+    jwglxt = Jwglxt()
+    jwglxt.login(username, password)
+    print(jwglxt.get_schedule(2017, 1))  # 获取个人课程表
